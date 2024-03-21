e11db30bd8b4e2e1287532eb7e2d874d864dc365
wg2 <khan.will1234@gmail.com>
2024-03-21 15:53:17 -0700

    Update Organization.py
    Adds comments and cleans up code.

--- Organization.py
+++ Organization.py
@@ -2,6 +2,14 @@ import CharacterRecognition
 import cv2
 from tkinter import *
 
+# Behavior: Creates a new window with the information about controls and
+#           loads the nutrition variables into the window
+# 
+# Exceptions: None
+#
+# Returns: None
+#
+# Parameters: A dictionary with the nutrtion labels inside
 def newWindow(map):
     window = Tk()
     window.title("Nutrition Label Display")
@@ -10,21 +18,32 @@ def newWindow(map):
     
     update(window, map)
 
+# Behavior: Updates the window with the new dictionary and resets it
+#
+# Exceptions: None
+#
+# Returns: None
+#
+# Parameters: A window object and a dictionary with the nutrition facts
 def update(window, map):
     i = 3
+
+    # Iterates through the dictionary and adds the contents as labels to the map
     for key in map:
         label = Label(window, text=key + ": " + str(map[key]))
         label.grid(row=i, column=0, columnspan=8)
         i += 1
+
+    # Displays the window
     window.mainloop()
 
+# Main method
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0)
     reader = CharacterRecognition
     cv2.namedWindow("test")
 
-
-    img_counter = 0
+    
     map = {
         "Calories": 0,
         "Total Fat": 0,
@@ -35,14 +54,14 @@ if __name__ == "__main__":
         "Total Carbohydrate": 0,
         "Dietary Fiber": 0,
         "Sugars": 0,
-        #"- Includes": 0,
         "Protein": 0,
         "Vitamin D": 0,
         "Calcium": 0,
         "Iron": 0,
         "Potassium": 0
     }
-    #window = newWindow(map)
+    
+    # Shows the camera and allows a user to read a label shown through their webcam
     while True:
         ret, frame = cam.read()
         if not ret:
@@ -50,15 +69,18 @@ if __name__ == "__main__":
             break
         cv2.imshow("test", frame)
 
+        # Gets what key was pressed
         k = cv2.waitKey(1)
+
+        # If it is esc it ends the program
         if k%256 == 27:
             # ESC pressed
             print("Escape hit, closing...")
             break
+        # If the key is space, it reads the label and opens a new window with the nutrition information
         elif k%256 == 32:
             # SPACE pressed
             reader.readNutritionLabel(image=frame, map=map)
-            #update(window=window, map=map)
             newWindow(map)
             map = {
                 "Calories": 0,
@@ -70,17 +92,12 @@ if __name__ == "__main__":
                 "Total Carbohydrate": 0,
                 "Dietary Fiber": 0,
                 "Sugars": 0,
-                #"- Includes": 0,
                 "Protein": 0,
                 "Vitamin D": 0,
                 "Calcium": 0,
                 "Iron": 0,
                 "Potassium": 0
             }
-            # img_name = "opencv_frame_{}.png".format(img_counter)
-            # cv2.imwrite(img_name, frame)
-            # print("{} written!".format(img_name))
-            img_counter += 1
 
 
     cam.release()
